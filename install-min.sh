911631d5f2935a83e2235d79f07effc20a0ea5a3
AJ ONeal <awesome@coolaj86.com>
2015-04-11 07:32:55 +0000

    nix config.json, but add sample as install step

---
+++ install-min.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TODO install from github and configure all keys correctly
+# TODO put some of the exchange logic into middleware?
+#rsync -av priv/config.sample.json priv/config.json
